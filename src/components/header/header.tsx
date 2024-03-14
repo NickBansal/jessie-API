f4b96b9ae44ba078c1a1e77df7c8f5e9f4157445
Nick Bansal <nick.bansal@autotrader.co.uk>
2024-03-14 13:24:22 +0000

    added in a navbar and hash util

---
+++ src/components/header/header.tsx
@@ -0,0 +1,5 @@
+import './header.css';
+
+export const Header = () => {
+  return <header>Music search generator</header>;
+};
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,20 +1,32 @@
+import { type SetStateAction, useEffect, useState } from 'react';
+
 import { ButtonLink } from '../components/buttons/buttons';
+import { Header } from '../components/header/header';
 import { AUTH_ENDPOINT, CLIENT_ID, REDIRECT_URI, RESPONSE_TYPE } from '../constants/spotify';
+import { windowsHash } from '../utils/window-hash';
 
 import './app.css';
 
 function App() {
+  const [token, setToken] = useState<SetStateAction<string | null>>('');
+
+  useEffect(() => {
+    const hashToken = windowsHash();
+
+    setToken(hashToken);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Music API generator</p>
-      </header>
+      <Header />
 
       <section className="App-section">
-        <ButtonLink
-          href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
-          btnText="Login to Spotify"
-        />
+        {!token && (
+          <ButtonLink
+            href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+            btnText="Login to Spotify"
+          />
+        )}
       </section>
     </div>
   );
