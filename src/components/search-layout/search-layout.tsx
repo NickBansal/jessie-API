963edfaadf0342811dc618c1a9d7dc6bf3469830
jessie pan <jessie.pan@autotrader.co.uk>
2024-03-21 16:13:41 +0000

    added the handle search type function

--- src/components/search-layout/search-layout.tsx
+++ src/components/search-layout/search-layout.tsx
@@ -5,11 +5,68 @@ import axios from 'axios';
 
 import { Spinner } from '../../components/spinner/spinner';
 import { Context } from '../../context/context';
-import { type SearchTypes } from '../../types/search-types';
 
+/* import { type SearchTypes } from '../../types/search-types'; */
 import './search-layout.css';
 
-function SearchLayout({ searchType }: SearchTypes) {
+interface Common {
+  name: string;
+  id: number;
+  popularity?: number;
+  artists?: Array<{
+    name: string;
+  }>;
+}
+
+interface ArtistsResult extends Common {
+  images: Array<{
+    url: string
+  }>;
+}
+
+interface TrackResult extends Common {
+  album: {
+    images: Array<{
+      url: string;
+    }>;
+  };
+  artists: Array<{
+    name: string;
+  }>;
+}
+
+interface AlbumResult extends Common {
+  images: Array<{
+    url: string
+  }>;
+  artists: Array<{
+    name: string;
+  }>;
+}
+
+interface AudiobooksResult extends Common {
+  authors: Array<{
+    name: string;
+  }
+  >;
+  total_chapters: number;
+  images: Array<{
+    url: string
+  }>;
+
+}
+
+interface ShowsResult extends Common {
+  total_episodes: number;
+  images: Array<{
+    url: string
+  }>;
+
+  media_type: string;
+
+}
+
+function SearchLayout({ searchType }: { searchType: string }) {
   const { token } = useContext(Context);
   const [results, setResults] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -39,20 +96,99 @@ function SearchLayout({ searchType }: SearchTypes) {
     }, 300);
   };
 
-  console.log(results, ' <<<<<<<');
+  console.log(results, searchType, ' <<<<<<<');
 
   return (
     <div className="App">
       <section className="App-section">
         {token ? (
-          <form onSubmit={searchParams}>
-            <i className="fa-sharp fa-solid fa-magnifying-glass fa-2xl" />
-            <input type="text" id="spotify-search-input" disabled={loading} />
-          </form>
-        ) : <p>Please login to use the search app</p>}
+          <div>
+            <form onSubmit={searchParams}>
+              <i className="fa-sharp fa-solid fa-magnifying-glass fa-2xl" />
+              <input type="text" id="spotify-search-input" disabled={loading} />
+            </form>
+
+          </div>
+        ) : <p className='warning-message'>Please login to use the search app</p>}
         {loading && <Spinner />}
 
         {/* Create components for the results */}
+
+        { results
+        ? <div className='result-box'>
+          <ul className='results'>
+          {searchType === "artists" ? results.map((result: ArtistsResult) => {
+            return (
+            <>
+            <li
+              key={result.id}
+              className='single-result'>
+                <div><img src={result.images[0].url} /></div>
+                <div className='name-genre'>
+                  <h3 className='result-name'>{result.name}</h3>
+                  <h4>Popularity: {result.popularity}</h4>
+                </div>
+            </li>
+            </>)
+          })
+          : searchType === 'albums' ? results.map((result: AlbumResult) => {
+            return (
+            <>
+            <li
+              key={result.id}
+              className='single-result'>
+                <div><img src={result.images[0].url} /></div>
+                <div className='name-genre'>
+                  <h3 className='result-name'>{result.name}</h3>
+                  <h4>Artist: {result.artists[0].name}</h4>
+                </div>
+            </li>
+            </>)
+          }) : searchType === 'tracks' ? results.map((result: TrackResult) => {
+            return (
+            <>
+            <li
+              key={result.id}
+              className='single-result'>
+                <div><img src={result.album.images[0].url} /></div>
+                <div className='name-genre'>
+                  <h3 className='result-name'>{result.name}</h3>
+                  <h4>Artist: {result.artists[0].name}</h4>
+                  <h4>Popularity: {result.popularity}</h4>
+                </div>
+            </li>
+            </>)
+          }) : searchType === 'audiobooks' ? results.map((result: AudiobooksResult) => {
+            return (
+            <>
+            <li
+              key={result.id}
+              className='single-result'>
+                <div><img src={result.images[0].url} /></div>
+                <div className='name-genre'>
+                  <h3 className='result-name'>{result.name}</h3>
+                  <h4>Author: {result.authors[0].name}</h4>
+                  <h4>Total Chapters: {result.total_chapters}</h4>
+                </div>
+            </li>
+            </>)
+          }) : searchType === 'shows' ? results.map((result: ShowsResult) => {
+            return (
+            <>
+            <li
+              key={result.id}
+              className='single-result'>
+                <div><img src={result.images[0].url} /></div>
+                <div className='name-genre'>
+                  <h3 className='result-name'>{result.name}</h3>
+                  <h4>Media Type: {result.media_type}</h4>
+                  <h4>Total Episodes: {result.total_episodes}</h4>
+                </div>
+            </li>
+            </>)
+          }) : <p>No results</p> }
+          </ul>
+        </div> : <p>No results</p>}
 
       </section>
     </div>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,35 @@
-import SearchLayout from '../components/search-layout/search-layout';
-import { Search } from '../types/search-types';
+import { type ChangeEvent, useState } from 'react';
 
+import SearchLayout from '../components/search-layout/search-layout';
+
+// import { Search } from '../types/search-types';
 import './app.css';
 
 function App() {
   // Add a toggle for different selection types
-  return <SearchLayout searchType={Search.ARTISTS} />;
+
+  const [searchType, setSearchTypes] = useState<any>('');
+  const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setSearchTypes(event.target.value);
+  }
+
+  return (
+  <>
+    <p className='choose-type'>Please choose the type you want to search for</p>
+    <select name='search-types' id='search-types' onChange={(e) => { handleChange(e); console.log(searchType); }}>
+      <option disabled selected hidden>Please choose the type</option>
+      <option value="artists">artists</option>
+      <option value="albums">albums</option>
+      <option value="tracks">tracks</option>
+      <option value="audiobooks">audiobooks</option>
+      <option value="shows">shows</option>
+    </select>
+    {searchType
+    ? <SearchLayout searchType={searchType} />
+    : null}
+
+  </>
+    )
 }
 
 export default App;
